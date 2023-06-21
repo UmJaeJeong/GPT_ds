c5c29abc2ec7858b0b72f1c1868bc4ceec9a442f
UmJaeJeong <39511137+UmJaeJeong@users.noreply.github.com>
2023-06-21 10:25:34 +0900

    GPT 샘플코드
    이전 대화 데이터 기록, 주고받는 대화방식 (while 사용)

---
+++ GPT_TEST/GptApi.py
@@ -0,0 +1,31 @@
+import os
+import openai
+
+#아래의 커맨드 입력후 openai 사용 가능
+#pip install openai 
+
+
+#환경설정에 등록된 key값 
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+
+#개인 key이므로 사용X
+openai.api_key = "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"
+
+messages = []
+while True:
+    user_content = input("사용자 : ")
+
+    messages.append({"role": "user", "content": f"{user_content}"})
+
+    #text-davinci-003
+    #gpt-3.5-turbo
+    completion = openai.ChatCompletion.create(model="text-davinci-003", messages=messages)
+  
+
+    assistant_content = completion.choices[0].message["content"].strip()
+
+    messages.append({"role": "assistant", "content": f"{assistant_content}"})
+
+    print(f"GPT : {assistant_content}")
+
+
