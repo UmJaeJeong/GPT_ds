f5099df3cc19c056460a0cf344db5f9120f504c6
UmJaeJeong <39511137+UmJaeJeong@users.noreply.github.com>
2023-06-22 18:13:40 +0900

    Add files via upload

--- GPT_TEST/GptFIneTuning.py
+++ GPT_TEST/GptFIneTuning.py
@@ -36,6 +36,9 @@ def fine_tune_model_delete(fine_tuned_model):
     print(f"*** 파인튜닝 삭제 ***\n{response}\n")
     return
 
+def fine_tune_cancel(model_name):
+    openai.FineTune.cancel(id=model_name)
+    return
 
 def fine_tuned_list_print(model_list):
     if model_list is not None:
@@ -45,24 +48,26 @@ def fine_tuned_list_print(model_list):
         print('The list is None')
 
 # # 파일 업로드 실행
-# file_path = "example1_prepared.jsonl"  # 업로드할 파일 경로
+# file_path = "example_prepared.jsonl"  # 업로드할 파일 경로
 # file_id = upload_file(file_path)
 # print(f"File uploaded successfully. File ID: {file_id}")
 
-# #업로된 파일 파인튜닝(학습)
+# # #업로된 파일 파인튜닝(학습)
 # training_file = file_id  # 학습 파일의 ID 또는 경로
 # response = fine_tune_model(training_file)
 
 #파인튜닝 목록 실행 / 튜닝모델만 파싱해서 출력
-mode_list = fine_tune_model_list()
+# mode_list = fine_tune_model_list()
+
 
 #파인튜닝 삭제 (1개)
-# fine_tune_model_delete(param)
+# fine_tune_model_delete('')
 
 #파인튜닝 리스트 전체 삭제 / 목록실행과 같이 사용 
-for fine_tuned_model in mode_list:
-    print(fine_tuned_model)
-    fine_tune_model_delete(fine_tuned_model)
+# for fine_tuned_model in mode_list:
+#     print(fine_tuned_model)
+#     # fine_tune_model_delete(fine_tuned_model)
+#     fine_tune_cancel(fine_tuned_model)
 
 
 # 응답 값 활용 예시
@@ -77,6 +82,16 @@ for fine_tuned_model in mode_list:
 
 
 # print("파인튜닝 목록")
+response = openai.FineTune.list()
+print(response)
 
-# response = openai.FineTune.list()
+
+#업로드 파일 리스트
+# response = JsonParse.JsonOutputUploadFileList(openai.File.list())
 # print(response)
+
+#파일삭제
+# for file in response:
+#     res = openai.File.delete(file)
+#     print(res)
+
