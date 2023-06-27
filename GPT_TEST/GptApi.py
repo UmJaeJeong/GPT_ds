733d31005feaac257dc0f36c5a5d0bd8d9fa9cf8
UmJaeJeong <39511137+UmJaeJeong@users.noreply.github.com>
2023-06-27 17:14:51 +0900

    Add files via upload

--- GPT_TEST/GptApi.py
+++ GPT_TEST/GptApi.py
@@ -5,14 +5,18 @@ import openai
 # openai.api_key = os.getenv("OPENAI_API_KEY")
 
 #Chat-GPT4로 했을때 채팅이 가능하고 / GPT3는 채팅이 불가능핟 그래서 히스토리를 사용행야함
-openai.api_key = "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"
+
+# OpenAI API 인증 설정
+openai.api_key = "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"
+
 
 
 def chat_with_model(message):
     response = openai.Completion.create(
-        model="curie:ft-personal-2023-06-23-02-11-24",
+        model="curie:ft-personal:umum-2023-06-27-06-14-50",
+        # model="curie:ft-personal-2023-06-23-02-11-24",
         prompt=message,
-        temperature=0.2,
+        temperature=0,
         max_tokens=256,
         top_p=1,
         frequency_penalty=0,
@@ -31,55 +35,14 @@ while True:
         break
     
     # 사용자 입력을 모델에 전달하여 응답 받기
-    assistant_content = str(chat_with_model(user_content)).split('\n')[0]
+    # assistant_content = str(chat_with_model(user_content)).split('\n')[0]
+    assistant_content = chat_with_model(user_content+" ->")
 
     
     messages.append({"role": "assistant", "content": f"{assistant_content}"})
 
     print(f"GPT : {assistant_content}")
-######################################################################################################3
 
-# messages = []
-# while True:
-#     user_content = input("사용자 : ")
-#     messages.append({"role": "user", "content": f"{user_content}"})
-
-#     # completion = openai.ChatCompletion.create(model="curie:ft-personal-2023-06-22-08-52-09", messages=messages)
-
-#     completion = openai.Completion.create(engine="curie:ft-personal-2023-06-22-08-52-09", messages=messages)
-
-
-#     assistant_content = completion.choices[0].message["content"].strip()
-
-#     messages.append({"role": "assistant", "content": f"{assistant_content}"})
-
-#     print(f"GPT : {assistant_content}")
-
-
-# def chat_with_gpt3(prompt):
-#     response = openai.Completion.create(
-#         engine="curie:ft-personal-2023-06-23-02-11-24",
-#         prompt=prompt,
-#         max_tokens=50,
-#         temperature=1.0,
-#         n=1,
-#         stop=None
-#     )
-#     if response and response.choices:
-#         return response.choices[0].text.strip()
-#     return None
-
-# print("AI: 안녕하세요! 대화를 시작해봅시다.")
-# while True:
-#     user_input = input("사용자: ")
-
-#     if user_input.lower() in ['exit', 'quit', 'bye']:
-#         print("AI: 채팅을 종료합니다.")
-#         break
-
-#     prompt = f"User: {user_input}\nAI:"
-#     response = chat_with_gpt3(prompt)
-#     print("AI:", response)
 
 
 ######################################################################################################3
--- GPT_TEST/GptFIneTuning.py
+++ GPT_TEST/GptFIneTuning.py
@@ -2,7 +2,14 @@ import openai
 from utils.JsonParse import JsonParse
 
 # OpenAI API 키 설정
-openai.api_key = ""
+openai.api_key = "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"
+
+#파인튜닝 작업 취소
+def fine_tune_cancel(file_id):
+    response = openai.FineTune.cancel(file_id)
+    print(response)
+
+#파인튜닝 정보 조회
 
 #학습파일 업로드 
 def upload_file(file_path):
@@ -10,15 +17,17 @@ def upload_file(file_path):
         response = openai.File.create(
             file=file,
             purpose='fine-tune'
-        )
+            )
         file_id = response['id']
         print(f"*** 학습파일업로드 ***\n{response}\n")
 
         return file_id
 
  #등록된 파일 파인튜닝   
-def fine_tune_model(training_file):
-    response = openai.FineTune.create(training_file=training_file)
+def fine_tune_model(training_file,suffix):
+    response = openai.FineTune.create(training_file=training_file,
+                                    suffix=suffix,
+                                    model="davinci")
     print(f"*** 등록된 파일 파인튜닝 ***\n{response}\n")
     return response
 
@@ -48,21 +57,44 @@ def fine_tuned_list_print(model_list):
     else:
         print('The list is None')
 
-# # 파일 업로드 실행
-file_path = "rater_prepared.jsonl"  # 업로드할 파일 경로
-file_id = upload_file(file_path)
-print(f"File uploaded successfully. File ID: {file_id}")
+    
+def failed_job_cancel():
+    # 모든 Fine-tuning 작업 ID 가져오기
+    response = openai.FineTune.list()
+    job_ids = [job["id"] for job in response["data"]]
 
-# # #업로된 파일 파인튜닝(학습)
-training_file = file_id  # 학습 파일의 ID 또는 경로
-response = fine_tune_model(training_file)
+    for job_id in job_ids:
+        # Fine-tuning 작업 상태 가져오기
+        job_response = openai.FineTune.retrieve(job_id)
+        status = job_response["status"]
+
+        if status == "failed" or "error" or "succeeded" in status:
+            # 실패한 작업 또는 오류가 발생한 작업은 건너뛰기
+            continue
+
+        # 작업 취소
+        cancel_response = openai.FineTune.cancel(job_id)
+        print(f"Cancelled job: {job_id}")
+
+        
+# fine_tune_cancel('ft-w4yfdRctLESLLozb9GOCH5rb')
+# failed_job_cancel()
+
+# # 파일 업로드 실행
+# file_path = "rater2_prepared.jsonl"  # 업로드할 파일 경로
+# file_id = upload_file(file_path)
+# print(f"File uploaded successfully. File ID: {file_id}")
+
+# #업로된 파일 파인튜닝(학습)
+# training_file = file_id  # 학습 파일의 ID 또는 경로
+# response = fine_tune_model(training_file,'um2')
 
 #여기까지만해서 파인튜닝 생성하면 playground에서 model명이 추가된것이 확인된다. 
 #모델명 ex) curie:ft-personal-2023-06-21-08-32-40
 #모델명 ex) davinci:ft-personal-2023-06-21-08-32-40
 
 # 파인튜닝 목록 실행 / 튜닝모델만 파싱해서 출력
-mode_list = fine_tune_model_list()
+# mode_list = fine_tune_model_list()
 
 
 #파인튜닝 삭제 (1개)
@@ -89,11 +121,11 @@ mode_list = fine_tune_model_list()
 # print(response)
 
 
-#업로드 파일 리스트
+# #업로드 파일 리스트
 # response = JsonParse.JsonOutputUploadFileList(openai.File.list())
 # print(response)
 
-#파일삭제
+# #파일삭제
 # for file in response:
 #     res = openai.File.delete(file)
 #     print(res)
