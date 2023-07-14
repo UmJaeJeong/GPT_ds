35baeb1d597085b8761535030433fa8213cea059
UmJaeJeong <39511137+UmJaeJeong@users.noreply.github.com>
2023-07-14 13:53:39 +0900

    Create test.py

---
+++ GPT_TEST/test.py
@@ -0,0 +1,68 @@
+import os
+import openai
+
+#환경설정에 등록된 key값 
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+
+#Chat-GPT4로 했을때 채팅이 가능하고 / GPT3는 채팅이 불가능핟 그래서 히스토리를 사용행야함
+
+# OpenAI API 인증 설정
+openai.api_key = "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"
+
+
+
+
+# def chat_with_model(message):
+#     response = openai.Completion.create(
+#         # model="curie:ft-personal:umum-2023-06-27-06-14-50",
+#         # model="curie:ft-personal-2023-06-23-02-11-24",ss
+#         # model="text-davinci-003",
+#         engine="text-davinci-003",
+#         prompt=message,
+#         temperature=0.2,
+#         max_tokens=256,
+#         top_p=1, 
+#         n=1,
+#         frequency_penalty=0,
+#         presence_penalty=0
+#     )
+#     return response.choices[0].text.strip()
+
+
+
+# # 대화 시작
+# print("모델과 대화를 시작합니다. 종료하려면 '종료'라고 입력하세요.")
+# messages = []
+# while True:
+#     user_content = input("사용자 : ")
+#     messages.append({"role": "user", "content": f"{user_content}->"})    
+
+#     if user_content == "종료":
+#         break
+    
+#     # 사용자 입력을 모델에 전달하여 응답 받기
+#     # assistant_content = str(chat_with_model(user_content)).split('\n')[0]
+#     assistant_content = chat_with_model(user_content)
+
+    
+#     messages.append({"role": "assistant", "content": f"{assistant_content}"})
+
+#     print(f"GPT : {assistant_content}")
+
+
+######################################################################################################3
+
+messages = []
+while True:
+    user_content = input("사용자 : ")
+    messages.append({"role": "user", "content": f"{user_content}"})
+
+    completion = openai.ChatCompletion.create( model="gpt-3.5-turbo", messages=messages)
+
+    assistant_content = completion.choices[0].message["content"].strip()
+
+    messages.append({"role": "assistant", "content": f"{assistant_content}"})
+
+    print(f"GPT : {assistant_content}")
+
+
